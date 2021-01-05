d9a0d38fad1cec8509322ce3a15ddd0b31f3841b
Stefaan De Vylder <stefaan.devylder@gmail.com>
2021-01-05 11:50:07 +0100

    Initial commit

---
+++ HetStrakPlan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HetStrakPlan
+//
+//  Created by Miranda Oszlanszky on 05/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
